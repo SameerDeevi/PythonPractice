6fa88160a1a4cd3b7ac5c053bb9218ffbb59f152
Sameer Kumar Deevi <sdeevi@buffalo.edu>
2018-01-07 21:32:59 -0800

    changes to first practice

--- First_Class_program.py
+++ First_Class_program.py
@@ -1,17 +1,17 @@
-class Car(Color, Make, Year, Price):
+class Car():
     Color = ''
     Make = ''
     Year  = ''
     Price = 0
-    def __repr__(self):
-        print('Car Class')
+    ##def __repr__(self):
+        ##print('Car Class')
 
     def __init__(self,Color, Make, Year, Price):
         self.Color = Color
         self.Make = Make
         self.Year = Year
-        Self.Price = Price
+        self.Price = Price
 
 Subaru = Car(Color = 'red', Make = 'Subaru', Year = '2018',Price = 30000)
 
-print(Subaru.data)
+print(Subaru.Color)
