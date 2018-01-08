e34fad9ef6d57d955ead4772dbfff985656a0693
Sameer Kumar Deevi <sdeevi@buffalo.edu>
2018-01-07 21:19:58 -0800

    First checkin

---
+++ First_Class_program.py
@@ -0,0 +1,17 @@
+class Car(Color, Make, Year, Price):
+    Color = ''
+    Make = ''
+    Year  = ''
+    Price = 0
+    def __repr__(self):
+        print('Car Class')
+
+    def __init__(self,Color, Make, Year, Price):
+        self.Color = Color
+        self.Make = Make
+        self.Year = Year
+        Self.Price = Price
+
+Subaru = Car(Color = 'red', Make = 'Subaru', Year = '2018',Price = 30000)
+
+print(Subaru.data)
